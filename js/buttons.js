1da5cd9ad365d669d86f6bd7cf77052290ddfa28
VabaLabaDubDub <119881539+VabaLabaDubDub@users.noreply.github.com>
2023-05-07 18:08:16 +0300

    qwqe

---
+++ js/buttons.js
@@ -0,0 +1,41 @@
+/*
+const regButton = document.createElement('button');
+regButton.className = 'registration';
+regButton.innerHTML = 'Click me';
+regButton.style.cssText = `
+width: 150px;
+height: 50px;
+border-radius: 6px;
+border: 1px;
+background-color:rgba(127, 255, 212, 0.74);
+font-size: 20px;
+margin-left: 43.333%;
+margin-top: 200px;
+`;
+*/
+
+
+document.body.append(regButton);
+
+const confirmButton = document.createElement('button');
+confirmButton.className = 'confirm';
+confirmButton.innerHTML = 'Ok'
+confirmButton.style.cssText = `
+width: 150px;
+height: 50px;
+border-radius: 6px;
+border: 1px;
+background-color:rgba(127, 255, 212, 0.74);
+font-size: 20px;
+`;
+
+const cancelButton = document.createElement('button');
+cancelButton.className = 'cancel';
+cancelButton.innerHTML = 'Cancel';
+cancelButton.style.cssText = `width: 150px;
+height: 50px;
+border-radius: 6px;
+border: 1px;
+background-color:rgba(127, 255, 212, 0.74);
+font-size: 20px;
+`;
---
+++ js/script.js
@@ -0,0 +1,9 @@
+const container = document.querySelector('.container');
+
+function openModal(){
+	document.getElementsByClassName('mdlwndw').style.display = 'block';
+};
+function closeModal(){
+	document.getElementsByClassName('mdlwndw').style.display = 'none';
+}
+
